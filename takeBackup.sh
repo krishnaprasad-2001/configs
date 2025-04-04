b2fa1c87a8103e871b89d6c7377f5c33a0c42c24
krishnaprasad <krishnaprasadpa2001@gmail.com>
2025-04-04 11:31:50 +0530

    Friday_04_April_2025_11_31_50_AM_IST

--- takeBackup.sh
+++ takeBackup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 backup_file=$(date |tr " " _ |tr ":" _)
+echo "$(backup_file): Backup started" >> /var/log/config-backup.log
 zip -r /root/config/config_$backup_file /home/krishna/.config
 cd /root/config
 git add . 
