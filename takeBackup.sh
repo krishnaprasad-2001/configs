da8feda5a34cbe7bc9ca313b803fdb1aab9e7c21
krishnaprasad <krishnaprasadpa2001@gmail.com>
2025-04-05 12:36:48 +0530

    Saturday_05_April_2025_12_36_48_PM_IST

--- takeBackup.sh
+++ takeBackup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 backup_file=$(date |tr " " _ |tr ":" _)
+rsync -avz /home/krishna/.tmux .
+rsync -avz /home/krishna/.tmux.conf .
 echo "$(backup_file): Backup started" >> /var/log/config-backup.log
 zip -r /root/config/config_$backup_file /home/krishna/.config
 cd /root/config
