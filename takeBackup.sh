4d63c4530ea6f6e6ee2e82a02e75e37339667965
krishnaprasad <krishnaprasadpa2001@gmail.com>
2025-04-05 12:45:45 +0530

    latest test commit

--- takeBackup.sh
+++ takeBackup.sh
@@ -7,5 +7,9 @@ echo "$(backup_file): Backup started" >> /var/log/config-backup.log
 zip -r /root/config/config_$backup_file /home/krishna/.config
 cd /root/config
 git add . 
+if [[ -t 1 ]]; then
+	read -p "Enter the commit name"
+	backup_file=$REPLY
+fi
 git commit -m "$backup_file"
 git push
